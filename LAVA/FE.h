7475a3a5aa58390e02a195400f25ee77b5209f62
tom mulvey <tommulvey5567@gmail.com>
2020-03-14 16:05:56 -0400

    finished main pg and moved into header filke

---
+++ LAVA/FE.h
@@ -0,0 +1,250 @@
+#pragma once
+#ifndef FE_H
+#define FE_H
+
+//#include <iostream>
+//#include "openssl/opensslv.h"
+//#include "clamav.h"
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <stdint.h>
+//#include <stdarg.h>
+//#include <string.h>
+//#include <math.h>
+//#include <assert.h>
+//#include <math.h>
+//#include <limits.h>
+//#include <time.h>
+//#include <GL/glew.h>
+//#include <GLFW/glfw3.h>
+//#include <SFML/OpenGL.hpp>
+//#include <SFML/Window.hpp>
+//
+//#define NK_INCLUDE_FIXED_TYPES
+//#define NK_INCLUDE_STANDARD_IO
+//#define NK_INCLUDE_STANDARD_VARARGS
+//#define NK_INCLUDE_DEFAULT_ALLOCATOR
+//#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+//#define NK_INCLUDE_FONT_BAKING
+//#define NK_INCLUDE_DEFAULT_FONT
+//#define NK_IMPLEMENTATION
+//#define NK_SFML_GL2_IMPLEMENTATION
+//
+//#include "nuklear.h"
+//#include "nuklear_sfml_gl2.h"
+//
+//#define STB_IMAGE_IMPLEMENTATION
+//#include "stb_image.h"
+//
+//#define WINDOW_WIDTH 1200
+//#define WINDOW_HEIGHT 800
+//#define IMG_COLOR nk_rgba(255, 255, 255, 255)
+
+struct pics {
+	const char* squareLogo;
+	const char* rectLogo;
+	const char* trapLogo;
+	const char* scan;
+	const char* history;
+};
+struct nk_command_buffer* canvas;
+struct nk_rect total_space;
+
+class FE
+{
+private:
+public:
+	/* CONSTRUCTORS */
+	FE(sf::Window *win);
+
+	/* MEMBER VARS */
+	struct nk_context* ctx;
+	struct nk_font_atlas* atlas;
+	struct nk_command_buffer* canvas;
+	struct nk_colorf bg;
+	struct nk_rect total_space;
+	pics pp;
+	struct nk_image scanImage;
+	struct nk_image rectImage; 
+	struct nk_image trapImage; 
+	struct nk_image squareImage; 
+	struct nk_image historyImage;
+	unsigned int view;
+	/*
+		0 : logo screen
+		1 : scans screen
+		2 : history screen
+		3 : 
+		4 :
+		5 : 
+		6 :
+	*/
+
+	/* MEMBER FUNCTIONS */
+	static struct nk_image icon_load(const char* filename, bool flip = false);
+	bool drawImage(struct nk_image *img);
+	bool DrawMainPage();
+	bool DrawScansPage();
+	bool Display();
+};
+inline struct nk_image FE::icon_load(const char* filename, bool flip)
+{
+	int x, y, n;
+	GLuint tex;
+	if (flip) {
+		stbi_set_flip_vertically_on_load(true);
+	}
+	unsigned char* data = stbi_load(filename, &x, &y, &n, STBI_rgb_alpha);
+	if (!data) std::cerr << "[SDL]: failed to load image\n";
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, x, y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(data);
+	return nk_image_id((int)tex);
+}
+
+inline bool FE::drawImage(struct nk_image *img)
+{
+	canvas = nk_window_get_canvas(this->ctx);
+	total_space = nk_window_get_content_region(this->ctx);
+	nk_draw_image(canvas, total_space, img, IMG_COLOR);
+	return true;
+}
+
+inline bool FE::Display() {
+	if (this->view == 0)
+		this->DrawMainPage();
+	if (this->view == 1)
+		this->DrawScansPage();
+
+	else {
+		std::cout << "ERRRRRRORRRRR\n";
+	}
+	return true;
+}
+
+inline bool FE::DrawMainPage()
+{
+	/* LOGO */
+	if (nk_begin(this->ctx, "lavalogo", nk_rect(25, 25, WINDOW_HEIGHT * .33, WINDOW_HEIGHT * .33),
+		NK_WINDOW_NO_SCROLLBAR)) {
+		this->drawImage(&this->squareImage);
+	}
+	nk_end(this->ctx);
+
+	/* SCAN ICON */
+	if (nk_begin(this->ctx, "scan", nk_rect(WINDOW_WIDTH * .2, WINDOW_HEIGHT * .49, WINDOW_HEIGHT * .3, WINDOW_HEIGHT * .3),
+		NK_WINDOW_NO_SCROLLBAR)) {
+		// hidden button to press behind icon
+		nk_layout_row_static(ctx, WINDOW_HEIGHT * .3, WINDOW_HEIGHT * .3, 1);
+		if (nk_button_label(ctx, "")) {
+			fprintf(stdout, "scans pressed\n");
+			this->view = 1;
+		}
+			
+		this->drawImage(&this->scanImage);
+	}
+	nk_end(this->ctx);
+
+	/* history ICON */
+	if (nk_begin(this->ctx, "history", nk_rect(WINDOW_WIDTH * .6, WINDOW_HEIGHT * .49, WINDOW_HEIGHT * .3, WINDOW_HEIGHT * .3),
+		NK_WINDOW_NO_SCROLLBAR)) {
+		// hidden button behind icon to press
+		nk_layout_row_static(ctx, WINDOW_HEIGHT * .33, WINDOW_HEIGHT * .33, 1);
+		if (nk_button_label(ctx, "")) {
+			fprintf(stdout, "history pressed\n");
+			this->view = 2;
+		}
+		this->drawImage(&this->historyImage);
+	}
+	nk_end(this->ctx);
+
+	return true;
+}
+
+inline bool FE::DrawScansPage()
+{
+	/* LOGO */
+	if (nk_begin(this->ctx, "lavalogo", nk_rect(25, 25, WINDOW_HEIGHT * .33, WINDOW_HEIGHT * .33),
+		NK_WINDOW_NO_SCROLLBAR)) {
+		this->drawImage(&this->squareImage);
+	}
+	nk_end(this->ctx);
+
+	/* SCAN ICON */
+	if (nk_begin(this->ctx, "scan", nk_rect(WINDOW_WIDTH * .2, WINDOW_HEIGHT * .49, WINDOW_HEIGHT * .3, WINDOW_HEIGHT * .3),
+		NK_WINDOW_NO_SCROLLBAR)) {
+		// hidden button to press behind icon
+		nk_layout_row_static(ctx, WINDOW_HEIGHT * .3, WINDOW_HEIGHT * .3, 1);
+		if (nk_button_label(ctx, "")) {
+			fprintf(stdout, "scans pressed\n");
+			this->view = 1;
+		}
+
+		this->drawImage(&this->scanImage);
+	}
+	nk_end(this->ctx);
+
+	/* history ICON */
+	if (nk_begin(this->ctx, "history", nk_rect(WINDOW_WIDTH * .6, WINDOW_HEIGHT * .49, WINDOW_HEIGHT * .3, WINDOW_HEIGHT * .3),
+		NK_WINDOW_NO_SCROLLBAR)) {
+		// hidden button behind icon to press
+		nk_layout_row_static(ctx, WINDOW_HEIGHT * .3, WINDOW_HEIGHT * .3, 1);
+		if (nk_button_label(ctx, "")) {
+			fprintf(stdout, "history pressed\n");
+			this->view = 2;
+		}
+		this->drawImage(&this->historyImage);
+	}
+	nk_end(this->ctx);
+
+	return true;
+}
+
+inline FE::FE(sf::Window *win) {
+	this->view = 0;
+
+	/* INIT IMAGES */
+	this->pp.scan = "../Assets/scan2.png";
+	this->pp.rectLogo = "../Assets/rectLogo.png";
+	this->pp.trapLogo = "../Assets/trapLogo.png";
+	this->pp.squareLogo = "../Assets/squareLogo.png";
+	this->pp.history = "../Assets/historylarger.png";
+
+	glewExperimental = 1;
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to setup GLEW\n");
+		std::cout << glewGetErrorString(glewInit());
+		exit(1);
+	}
+
+	/* GUI */
+	//struct nk_context* ctx;
+	this->ctx = nk_sfml_init(win);
+	// load font here if want
+	// load cursor here if want
+	//struct nk_font_atlas* atlas;
+	nk_sfml_font_stash_begin(&this->atlas);
+	nk_sfml_font_stash_end();
+
+	// load some image stuff
+	//struct nk_buffer cmds;
+	////nk_buffer_init_default(&cmds);
+	glEnable(GL_TEXTURE_2D);
+
+	// bg color
+	struct nk_colorf bg;
+	this->bg.r = 45 / 255.0f; this->bg.g = 45 / 255.0f; this->bg.b = 45 / 255.0f; this->bg.a = 1.0f;
+
+	this->scanImage = this->icon_load(pp.scan);
+	this->squareImage = this->icon_load(pp.squareLogo);
+	this->historyImage = this->icon_load(pp.history);
+}
+
+#endif
--- LAVA/main.cpp
+++ LAVA/main.cpp
@@ -1,6 +1,3 @@
-// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include "openssl/opensslv.h"
 #include "clamav.h"
@@ -16,7 +13,6 @@
 #include <time.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-
 #include <SFML/OpenGL.hpp>
 #include <SFML/Window.hpp>
 
@@ -40,43 +36,10 @@
 #define WINDOW_HEIGHT 800
 #define IMG_COLOR nk_rgba(255, 255, 255, 255)
 
-static struct nk_image
-icon_load(const char* filename, bool flip = false) {
-	int x, y, n;
-	GLuint tex;
-	if (flip) {
-		stbi_set_flip_vertically_on_load(true);
-	}
-	unsigned char* data = stbi_load(filename, &x, &y, &n, STBI_rgb_alpha);
-	if (!data) std::cerr << "[SDL]: failed to load image\n";
-	glGenTextures(1, &tex);
-	glBindTexture(GL_TEXTURE_2D, tex);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_NEAREST);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, x, y, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(data);
-	return nk_image_id((int)tex);
-}
+#include "FE.h"
 
-struct nk_command_buffer* canvas;
-
-const unsigned int WIDTH_IMG = 80;
-const unsigned int HEIGHT_IMG = 80;
-
-unsigned char imageData[WIDTH_IMG * HEIGHT_IMG * 4];
-
-struct nk_rect total_space;
-
-struct pics {
-	const char* squareLogo;
-	const char* rectLogo;
-	const char* trapLogo;
-	const char* scan;
-};
+//struct nk_command_buffer* canvas;
+//struct nk_rect total_space;
 
 int main()
 {
@@ -96,130 +59,35 @@ int main()
 	else {
 		printf("initialization successful");
 	}
-
-	/* INIT IMAGES */
-	pics pp;
-	struct nk_image scanImage;  pp.scan = "../Assets/scan2.png";
-	struct nk_image rectImage; pp.rectLogo = "../Assets/rectLogo.png";
-	struct nk_image trapImage; pp.trapLogo = "../Assets/trapLogo.png";
-	struct nk_image squareImage; pp.squareLogo = "../Assets/squareLogo.png";
-
+	
 	/* Platform */
 	sf::ContextSettings settings(24, 8, 4, 2, 2);
-	sf::Window win(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "LAVA", sf::Style::Default, settings);
+	sf::Window win(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "LAVA", sf::Style::Close, settings);
 	win.setVerticalSyncEnabled(true);
 	win.setActive(true);
 	glViewport(0, 0, win.getSize().x, win.getSize().y);
 
-	glewExperimental = 1;
-	if (glewInit() != GLEW_OK) {
-		fprintf(stderr, "Failed to setup GLEW\n");
-		std::cout << glewGetErrorString(glewInit());
-		exit(1);
-	}
+	FE FrontEnd = FE(&win);
 
-	/* GUI */
-	struct nk_context* ctx;
-	ctx = nk_sfml_init(&win);
-	// load font here if want
-	// load cursor here if want
-	struct nk_font_atlas* atlas;
-	nk_sfml_font_stash_begin(&atlas);
-	nk_sfml_font_stash_end();
-
-	// load some image stuff
-	struct nk_buffer cmds;
-	nk_buffer_init_default(&cmds);
-	glEnable(GL_TEXTURE_2D);
-
-
-	// bg color
-	struct nk_colorf bg;
-	bg.r = 45 / 255.0f; bg.g = 45 / 255.0f; bg.b = 45 / 255.0f; bg.a = 1.0f;
-
-
-	scanImage = icon_load(pp.scan);
-	squareImage = icon_load(pp.squareLogo);
 	while (win.isOpen())
 	{
 		/* Input */
 		sf::Event evt;
-		nk_input_begin(ctx);
+		nk_input_begin(FrontEnd.ctx);
 		while (win.pollEvent(evt)) {
 			if (evt.type == sf::Event::Closed)
 				win.close();
-			else if (evt.type == sf::Event::Resized)
-				glViewport(0, 0, evt.size.width, evt.size.height);
 			nk_sfml_handle_event(&evt);
 		}
-		nk_input_end(ctx);
+		nk_input_end(FrontEnd.ctx);
 
 		/* GUI */
-		/*glEnable(GL_TEXTURE_2D);
-		if (nk_begin(ctx, "LAVA", nk_rect(500, 500, 230, 250),
-			NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE |
-			NK_WINDOW_MINIMIZABLE | NK_WINDOW_TITLE))
-		{
-			enum { EASY, HARD };
-			static int op = EASY;
-			static int property = 20;
-			nk_layout_row_static(ctx, 30, 80, 1);
-			if (nk_button_label(ctx, "button"))
-				fprintf(stdout, "button pressed\n");
-
-			nk_layout_row_dynamic(ctx, 30, 2);
-			if (nk_option_label(ctx, "easy", op == EASY)) op = EASY;
-			if (nk_option_label(ctx, "hard", op == HARD)) op = HARD;
-
-			nk_layout_row_dynamic(ctx, 25, 1);
-			nk_property_int(ctx, "Compression:", 0, &property, 100, 10, 1);
-
-			nk_layout_row_dynamic(ctx, 20, 1);
-			nk_label(ctx, "background:", NK_TEXT_LEFT);
-			nk_layout_row_dynamic(ctx, 25, 1);
-			if (nk_combo_begin_color(ctx, nk_rgb_cf(bg), nk_vec2(nk_widget_width(ctx), 400))) {
-				nk_layout_row_dynamic(ctx, 120, 1);
-				bg = nk_color_picker(ctx, bg, NK_RGBA);
-				nk_layout_row_dynamic(ctx, 25, 1);
-				bg.r = nk_propertyf(ctx, "#R:", 0, bg.r, 1.0f, 0.01f, 0.005f);
-				bg.g = nk_propertyf(ctx, "#G:", 0, bg.g, 1.0f, 0.01f, 0.005f);
-				bg.b = nk_propertyf(ctx, "#B:", 0, bg.b, 1.0f, 0.01f, 0.005f);
-				bg.a = nk_propertyf(ctx, "#A:", 0, bg.a, 1.0f, 0.01f, 0.005f);
-				nk_combo_end(ctx);
-			}
-		}
-		nk_end(ctx);
-		*/
-
-
-		/* LOGO */
-		if (nk_begin(ctx, "lavalogo", nk_rect(25, 25, WINDOW_HEIGHT * .33, WINDOW_HEIGHT * .33),
-			NULL))
-		{
-			canvas = nk_window_get_canvas(ctx);
-			total_space = nk_window_get_content_region(ctx);
-
-
-			nk_draw_image(canvas, total_space, &squareImage, IMG_COLOR);
-		}
-		nk_end(ctx);
-
-		/* SCAN ICON */
-		if (nk_begin(ctx, "scan", nk_rect(WINDOW_WIDTH * .2, WINDOW_HEIGHT * .49, WINDOW_HEIGHT * .3, WINDOW_HEIGHT * .3),
-			NULL))
-		{
-			canvas = nk_window_get_canvas(ctx);
-			total_space = nk_window_get_content_region(ctx);
-
-			//myImage = p.scan;
-			nk_draw_image(canvas, total_space, &scanImage, IMG_COLOR);
-		}
-		nk_end(ctx);
+		FrontEnd.Display();
 
 		/* Draw */
 		win.setActive(true);
 		glClear(GL_COLOR_BUFFER_BIT);
-		glClearColor(bg.r, bg.g, bg.b, bg.a);
+		glClearColor(FrontEnd.bg.r, FrontEnd.bg.g, FrontEnd.bg.b, FrontEnd.bg.a);
 		/* IMPORTANT: `nk_sfml_render` modifies some global OpenGL state
 		* with blending, scissor, face culling and depth test and defaults everything
 		* back into a default state. Make sure to either save and restore or
